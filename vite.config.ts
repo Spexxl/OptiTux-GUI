2f9a5f4ecadb62827a66c65dfbb413514d8b765a
Spexxl <snexxlorioh@outlook.com>
2026-04-17 23:31:46 -0300

    refactor: simplify vite build configuration by removing conditional targets and environment-based settings

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-  // 1. O Tauri espera que os caminhos sejam relativos no bundle final
+
   base: "",
   define: {
     APP_VERSION: JSON.stringify(pkg.version),
@@ -22,7 +22,7 @@ export default defineConfig(async () => ({
     },
   },
 
-  // 2. Otimizações específicas para o Tauri v2
+  
   clearScreen: false,
   server: {
     port: 1420,
@@ -39,11 +39,10 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
-  // 3. Garante que o build suporte browsers mais antigos se necessário
+ 
   build: {
-    target: process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
-    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
-    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+    target: "es2020",
+    minify: "esbuild",
+    sourcemap: false,
   },
 }));
-
